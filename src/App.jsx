b95921e4797f6276f54ef0d35a70866642263672
Rahul Mishra <rahulmishra21791@gmail.com>
2024-09-12 10:58:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import AutoComplete from "./auto-complete";
+
+const Suggestions = ["Apple", "Mango", "Banana", "Kiwi", "Grapes"];
+
+function App() {
+  const [suggestions, setSuggestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(Suggestions);
+      }, 3000);
+    }).then((data) => {
+      setSuggestions(data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <AutoComplete suggestions={suggestions} isLoading={isLoading} />
+    </>
+  );
+}
+
+export default App;
